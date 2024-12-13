c19bfc35b0505bfa91f6a08cca7245149f402811
Wahab Hussain <hellowahab@gmail.com>
2024-12-13 21:55:43 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.inserting_items_into_an_array.Duplicate_Zeros;
+using System.Diagnostics;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Duplicate Zeros!");
+			var duplicateZeros = new DuplicateZeros();
+            int[] arr = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			duplicateZeros.FirstSolution(arr);
+            stopwatch.Stop();
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            Console.WriteLine($"Execution Time: {timeSpan.Minutes}m {timeSpan.Seconds}s {timeSpan.Milliseconds}ms {timeSpan.Nanoseconds}ns");
+			Console.ReadLine();
+		}
+    }
+}
---
+++ inserting-items-into-an-array/Duplicate Zeros/DuplicateZeros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.inserting_items_into_an_array.Duplicate_Zeros
+{
+	internal class DuplicateZeros
+	{
+		/// <summary>
+		/// Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+		/// This is the first solution I have tried an come up with.
+		/// </summary>
+		/// <param name="arr"></param>
+		public void FirstSolution(int[] arr)
+		{
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				if (arr[i] == 0)
+				{
+					for (int j = arr.Length - 1; j > i; j--)
+						arr[j] = arr[j - 1];
+					arr[i + 1] = 0;
+					i++;
+				}
+			}
+
+			Console.WriteLine(string.Join(", ", arr));
+		}
+	}
+}
